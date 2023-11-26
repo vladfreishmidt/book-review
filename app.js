f7c19a38d9e00688be5b427f79ee3491b091bbdf
Vladyslav Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-11-26 16:39:40 +0200

    Add initial app structure by MVC

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 
 const app = express();
+const router = require("./router");
+
+// Accept data
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(express.static("public"));
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
-  res.render("home-guest");
-});
+app.use("/", router);
 
 app.listen(3000, function () {
   console.log("Listening on 3000");
