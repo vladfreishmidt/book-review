2a2c81f8de0fcd5e4e34a8cea41f45072a91a993
Vladyslav Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-11-27 22:11:40 +0200

    Hash user password

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const usersCollection = require("../db").collection("users");
 const validator = require("validator");
 
@@ -51,8 +52,8 @@ User.prototype.validate = function () {
     this.errors.push("Password must be at least 12 characters.");
   }
 
-  if (this.data.password.length > 100) {
-    this.errors.push("Password cannot exceed 100 characters.");
+  if (this.data.password.length > 50) {
+    this.errors.push("Password cannot exceed 50 characters.");
   }
 
   if (this.data.username.length > 0 && this.data.username.length < 3) {
@@ -72,6 +73,9 @@ User.prototype.register = function () {
   // Step #2: If no validation error
   // then save the user data into a database
   if (!this.errors.length) {
+    let salt = bcrypt.genSaltSync(10);
+    this.data.password = bcrypt.hashSync(this.data.password, salt);
+
     usersCollection.insertOne(this.data);
   }
 };
@@ -85,7 +89,10 @@ User.prototype.login = function () {
       username: this.data.username,
     });
 
-    if (attemptedUser && attemptedUser.password === this.data.password) {
+    if (
+      attemptedUser &&
+      bcrypt.compareSync(this.data.password, attemptedUser.password)
+    ) {
       resolve("Congrats");
     } else {
       reject("Invalid username / password");
