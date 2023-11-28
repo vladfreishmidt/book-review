5f13e07dca0cadd83367724116805e97e4ac9be4
Vladyslav Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-11-28 14:38:53 +0200

    Add sessions config

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
 
 const app = express();
+
+let sessionsOptions = session({
+  secret: "JavaScript is so cool",
+  store: MongoStore.create({ client: require("./db") }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24, // 24h
+    httpOnly: true,
+  },
+});
+
+app.use(sessionsOptions);
+app.use(flash());
+
 const router = require("./router");
 
 // Accept data
